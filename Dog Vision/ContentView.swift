9f2a63d00caee48edeea3e24b7c089aee17bd04b
Saju Pillai <saju.pillai@gmail.com>
2022-07-09 18:31:35 -0700

    Initial Commit

---
+++ Dog Vision/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dog Vision
+//
+//  Created by Saju Pillai on 7/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dog Vision/Dog_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_VisionApp.swift
+//  Dog Vision
+//
+//  Created by Saju Pillai on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Dog_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
